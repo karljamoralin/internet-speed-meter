0315cc457e79b6720a7bc602ec1f981dddd0a96b
Karl Jamoralin <karl.jamoralin@gmail.com>
2016-08-30 00:54:41 +0800

    Changed to material theme Added switch

--- app/src/main/java/com/karljamoralin/internetspeedmeter/MainActivity.java
+++ app/src/main/java/com/karljamoralin/internetspeedmeter/MainActivity.java
@@ -1,9 +1,10 @@
 package com.karljamoralin.internetspeedmeter;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
